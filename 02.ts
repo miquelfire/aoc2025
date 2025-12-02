66ff7110282ae644bce2d02089c474f8cd9e8499
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-02 12:28:44 -0500

    Day 2 Part 2 done

--- 02.ts
+++ 02.ts
@@ -49,7 +49,15 @@ export const part1 = async (d: string) => {
 };
 
 export const part2 = async (d: string) => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const ranges = d.split(/,[\s]*/).map(e => e.split('-').map(e => +e));
+	const regex = /^(.+?)\1+$/;
+	let sum = 0n;
+	ranges.forEach(e => {
+		for (let i = e[0]; i <= e[1]; i++) {
+			if (regex.test('' + i)) {
+				sum += BigInt(i);
+			}
+		}
+	});
+	return sum.toString();
 };
