d7efbe93a348e142893275d221d056f8651e1934
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-04 00:22:33 -0500

    Day 4 Part 2 done.

--- 04.ts
+++ 04.ts
@@ -30,7 +30,45 @@ export const part1 = async (d: string) => {
 };
 
 export const part2 = async (d: string) => {
-	const data = d.split('\n');
-	data.splice(0, data.length);
-	return data;
+	const data = d.split('\n').map(e => e.split(''));
+	let sum = 0;
+	const grid = new Set<string>();
+	data.forEach((row, y) => {
+		row.forEach((cell, x) => {
+			if (cell == '@') {
+				grid.add(y + 'x' + x);
+				data[y][x] = '.';
+			}
+		});
+	});
+
+	let removedRolls = true;
+	while (removedRolls) {
+		removedRolls = false;
+		const rollsToRemove = new Set<string>();
+		grid.forEach(cell => {
+			const [y, x] = cell.split('x').map(e => parseInt(e));
+			let neighbors = 0;
+			for (let ty = y - 1; ty < y + 2; ty++) {
+				for (let tx = x - 1; tx < x + 2; tx++) {
+					if (ty != y || tx != x) {
+						if (grid.has([ty, tx].join('x'))) {
+							neighbors++;
+						}
+					}
+				}
+			}
+			if (neighbors < 4) {
+				sum++;
+				rollsToRemove.add(cell);
+				removedRolls = true;
+			}
+		});
+		rollsToRemove.forEach(cell => {
+			grid.delete(cell);
+		});
+	
+	}
+
+	return sum;
 };
