64bba8484471c154af5249ce6354b0533b43c2fa
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-05 13:04:57 -0500

    Just pre-adding all the days

---
+++ 10.ts
@@ -0,0 +1,10 @@
+export const part1 = async (d: string) => {
+	const data = d.split('\n');
+	return data;
+};
+
+export const part2 = async (d: string) => {
+	const data = d.split('\n');
+	data.splice(0, data.length);
+	return data;
+};
---
+++ 12.ts
@@ -0,0 +1,8 @@
+export const part1 = async (d: string) => {
+	const data = d.split('\n');
+	return data;
+};
+
+export const part2 = async (d: string) => {
+	return d;
+};
