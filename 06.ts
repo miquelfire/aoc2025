e54a5cc1970db4f4fa3c87c5e9cf32a389e867d6
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-06 00:20:19 -0500

    Day 6 part 1 Done

--- 06.ts
+++ 06.ts
@@ -1,6 +1,19 @@
 export const part1 = async (d: string) => {
-	const data = d.split('\n');
-	return data;
+	const data = d.split('\n').map(e => e.trim().split(/ +/));
+	const commands = data.pop();
+	const numbers = data.map(e => e.map(e => BigInt(e)));
+	const subtotals = numbers.shift();
+	numbers.forEach(row => row.forEach((number, col) => {
+		switch (commands[col]) {
+			case '+':
+				subtotals[col] += number;
+				break;
+			case '*':
+				subtotals[col] *= number;
+				break;
+		}
+	}));
+	return subtotals.reduce((p,v) => p + v, 0n).toString();
 };
 
 export const part2 = async (d: string) => {
