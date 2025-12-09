aa4c380667dcc62dbc72bf898d8f0c4832b2f754
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-09 10:46:00 -0500

    Day 9 Part 1 done

--- 09.ts
+++ 09.ts
@@ -1,6 +1,15 @@
 export const part1 = async (d: string) => {
-	const data = d.split('\n');
-	return data;
+	const list = d.split('\n').map(e => e.split(',').map(e => parseInt(e)));
+	let maxArea = 0;
+	for (let i = 0; i < list.length; i++) {
+		const [x1, y1] = list[i];
+		for (let j = i + 1; j < list.length; j++) {
+			const [x2, y2] = list[j];
+			const area = (1 + Math.abs(x2 - x1)) * (1 + Math.abs(y2 - y1));
+			maxArea = Math.max(area, maxArea);
+		}
+	}
+	return maxArea;
 };
 
 export const part2 = async (d: string) => {
