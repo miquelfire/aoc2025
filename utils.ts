94af3a4dda5fde8fd64179cd8be6b9c924e6ee1f
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-12 18:01:46 -0500

    Added memorize function

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 export class PriorityQueue<T> {
-	values: {priority: number, node: T}[];
+	values: { priority: number, node: T }[];
 	constructor() {
 		this.values = [];
 	}
@@ -70,7 +70,7 @@ export function ucs(startNode: string, endNode: string, graph: Map<string, { nod
 	cost_so_far.set(startNode, 0);
 
 	while (queue.size > 0) {
-		const {node: currentNode} = queue.get();
+		const { node: currentNode } = queue.get();
 		if (currentNode == endNode) break;
 
 		// Search side paths
@@ -106,4 +106,40 @@ export function gcd(a: number, b: number) {
 
 export function lcm(a: number, b: number) {
 	return a / gcd(a, b) * b;
+}
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+type AnyFunction = (...args: any[]) => any;
+
+interface MemoizedFunction<T extends AnyFunction> extends CallableFunction {
+	(...args: Parameters<T>): ReturnType<T>;
+	clear: () => void;
+}
+
+export function memoize<T extends AnyFunction>(fn: T): MemoizedFunction<T> {
+	const cache = new Map<string, ReturnType<T>>();
+
+	const memoizedFunction = function (...args: Parameters<T>): ReturnType<T> {
+		const key = JSON.stringify(args);
+
+		if (cache.has(key)) {
+			return cache.get(key)!;
+		}
+
+		const result = fn(...args);
+		cache.set(key, result);
+
+		return result;
+	} as MemoizedFunction<T>;
+
+	memoizedFunction.clear = function clear() {
+		cache.clear();
+	};
+
+	Object.defineProperty(memoizedFunction, 'name', {
+		value: `memoized_${fn.name}`,
+		configurable: true
+	});
+
+	return memoizedFunction;
 }
