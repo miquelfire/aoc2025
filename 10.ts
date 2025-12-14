a1267f64e2143b3b23b8655d2a3a359dc220b4de
Miquel Fire Burns <miquelfire@gmail.com>
2025-12-13 22:57:48 -0500

    Day 10 Part 1 done

--- 10.ts
+++ 10.ts
@@ -35,27 +35,31 @@ function parseInput(input: string) {
 
 export const part1 = async (d: string) => {
 	const machines = d.split('\n').map(e => parseInput(e));
+	let combos = 0;
 	machines.forEach(machine => {
-		const buttonIdx = machine.lights.map((e, i) => (e == 1) ? i: -1).filter(e => e != -1).join(',');
-		const buttonsPatternLengthTest = (2 ** machine.buttons.length) - 1;
-		
-		console.log(`===${buttonIdx}===`);
-		for (let j = 1; j <= buttonsPatternLengthTest; j ++) {
-			const buttonsToPress = [...Array(machine.buttons.length)].map((x,i)=>j>>i&1);
-			console.log(buttonsToPress);
-		}
-		/*
-		void add1(int *a, int len) {
-			int carry = 1;
-			for (int i = len - 1; carry > 0 && i >= 0; i--) {
-			  int result = a[i] + carry;
-			  carry = result >> 1;
-			  a[i] = result & 1;
+		const buttonIdx = machine.lights.map((e, i) => (e == 1) ? i : -1).filter(e => e != -1).join(',');
+		const buttonCount = machine.buttons.length;
+		const buttonsPatternLengthTest = (2 ** buttonCount) - 1;
+		let minButtons = buttonCount;
+
+		for (let j = 1; j <= buttonsPatternLengthTest; j++) {
+			const buttonsToPress = [...Array(buttonCount)].map((x, i) => j >> i & 1);
+			const buttonsToggled = Array.from(machine.lights, () => 0);
+			for (let i = 0; i < buttonsToPress.length; i++) {
+				if (buttonsToPress[i]) {
+					for (const button of machine.buttons[i]) {
+						buttonsToggled[button] ^= 1;
+					}
+				}
 			}
-		  }
-		*/
+			const lights = buttonsToggled.map((e, i) => (e == 1) ? i : -1).filter(e => e != -1).join(',');
+			if (lights == buttonIdx) {
+				minButtons = Math.min(minButtons, buttonsToPress.filter(e => e).length);
+			}
+		}
+		combos += minButtons;
 	});
-	return machines;
+	return combos;
 };
 
 export const part2 = async (d: string) => {
